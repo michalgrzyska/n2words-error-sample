a49b79348b3a064da4f943d875bdfe9a9ce78756
Michał Grzyśka <grzyska.michal@gmail.com>
2023-10-23 19:48:28 +0200

    Initial

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+//import n2words from "n2words";
+
+function moneyToWords(money: number, lang: "pl" | "en"): string {
+  return ""; //n2words(money, { lang });
+}
+
+const test = moneyToWords(1000, "en");
+console.log(test);
